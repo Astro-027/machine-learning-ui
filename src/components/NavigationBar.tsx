f9e0593087fc9897cedc46cb945cb0eb64e5cf79
Nabeyou Tadessa <nabeyoutadessa@gmail.com>
2024-06-11 09:27:52 -0700

    Added all components to HomePage to make it single page website

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -6,10 +6,10 @@ const NavigationBar: React.FC = () => {
         <nav className="navbar">
             <div className="navbar-links">
                 <Link to="/" className="nav-link">Home</Link>
-                <Link to="/data-selection" className="nav-link">Data Selection</Link>
+                {/* <Link to="/data-selection" className="nav-link">Data Selection</Link>
                 <Link to="/model-configuration" className="nav-link">Model Configuration</Link>
                 <Link to="/results-visualization" className="nav-link">Results Visualization</Link>
-                <Link to="/help" className="nav-link">Help/FAQ</Link>
+                <Link to="/help" className="nav-link">Help/FAQ</Link> */}
             </div>
         </nav>
     );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import WelcomeSection from '../components/WelcomeSection';
 import QuickStartGuide from '../components/QuickStartGuide';
 import LoginRegistration from '../components/LoginRegistration';
+import DataSetLibrary from '../components/DataSetLibrary';
+import DataUpload from '../components/DataUpload';
+import DataPreview from '../components/DataPreview';
+import ModelSelection from '../components/ModelSelection';
+import ModelArchitectureSettings from '../components/ModelArchitectureSettings';
+import ModelTrainingSettings from '../components/ModelTrainingSettings';
+import ResultsDisplay from '../components/ResultsDisplay';
+import ModelComparison from '../components/ModelComparison';
+import StatisticsMetrics from '../components/StatisticsMetrics';
+import FAQSection from '../components/FAQSection';
+import TutorialSection from '../components/TutorialSection';
+import ContactSupport from '../components/ContactSupport';
+import FeedbackForm from '../components/FeedbackForm';
+
 
 const HomePage: React.FC = () => {
     return (
@@ -9,6 +23,23 @@ const HomePage: React.FC = () => {
             <WelcomeSection />
             <QuickStartGuide />
             <LoginRegistration />
+            <h1>Data Selection</h1>
+            <DataSetLibrary />
+            <DataUpload />
+            <DataPreview />
+            <h1>Model Configuration</h1>
+            <ModelSelection />
+            <ModelArchitectureSettings />
+            <ModelTrainingSettings />
+            <h1>Results Visualization</h1>
+            <ResultsDisplay />
+            <ModelComparison />
+            <StatisticsMetrics />
+            <h1>Help & FAQ</h1>
+            <FAQSection />
+            <TutorialSection />
+            <ContactSupport />
+            <FeedbackForm />
         </div>
     );
 };
