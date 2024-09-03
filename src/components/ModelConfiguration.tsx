f36775b1fc08533129887cf5dbdb070a08d3ae74
chunhez <45885907+chunhez@users.noreply.github.com>
2024-09-02 23:56:13 -0400

    Added flask backend

--- src/components/ModelConfiguration.tsx
+++ src/components/ModelConfiguration.tsx
@@ -1,11 +1,43 @@
 // ModelConfiguration.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ModelSelection from './ModelSelection';
 import ModelArchitectureSettings from './ModelArchitectureSettings';
 import ModelTrainingSettings from './ModelTrainingSettings';
 import './styles/ModelConfiguration.css';
 
 const ModelConfiguration: React.FC = () => {
+  const [data, setdata] = useState('Stop');   
+  const [clicked, setClicked] = useState(false);
+  
+  function handleStream(e:any){
+	      console.log(e)
+	      setdata(e.data)
+	    };
+  
+  useEffect(() => {
+  
+  if (clicked) {
+  	    
+  	    setdata('Training')
+	    const sse = new EventSource('http://localhost:5000/stream');
+
+	    sse.onmessage = e =>{handleStream(e)};
+
+	    sse.onerror = e => {
+	      //GOTCHA - can close stream and 'stall'
+	      sse.close()
+	    };
+
+	    return () => {
+	      sse.close();
+	      
+	    };
+	    };
+    }, [clicked]);
+  
+  
+  
+  
   return (
     <div className="model-configuration">
       <h2>Model Configuration</h2>
@@ -24,7 +56,8 @@ const ModelConfiguration: React.FC = () => {
       <div className="model-section">
         <ModelTrainingSettings />
       </div>
-      <button className="apply-settings">Apply Settings  and Train</button>
+      <button onClick={() => setClicked(true)}>Apply Settings  and Train</button>
+      <p>Status: {data}</p>
     </div>
   );
 };
